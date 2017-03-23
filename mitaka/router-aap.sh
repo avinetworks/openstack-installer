d291f0da8b05369cc55bd082d2dd7f44cb5ffda3
Praveen Yalagandula <ypraveen@avinetworks.com>
2017-03-23 19:36:21 +0000

    Update to aap script; also added a custom post install inscript

--- mitaka/router-aap.sh
+++ mitaka/router-aap.sh
@@ -11,6 +11,9 @@ fi
 
 # figure out the port-id from lab credentials
 source ./lab_openrc.sh
+# add openstack-controller to /etc/hosts
+sed -i "s/10.10.16.82 openstack-controller\n//g" /etc/hosts
+echo "10.10.16.82 openstack-controller" >> /etc/hosts
 port_id=`neutron port-list | grep "$my_mac" | awk '{print $2;}'`
 
 

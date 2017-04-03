74148d355faca7093640dab25b98223c22a73289
Praveen Yalagandula <ypraveen@avinetworks.com>
2017-04-03 22:36:44 +0000

    Command line argument to supply avi controller IP for lbaas driver install

--- others/add-avi-lbaasv2-driver.sh
+++ others/add-avi-lbaasv2-driver.sh
@@ -2,6 +2,9 @@ set -e
 set -x
 
 avi_cntr_ip="10.10.39.219"
+if [ $# -gt 0 ]; then
+   avi_cntr_ip=$1
+fi
 avi_admin_passwd="avi123\$%"
 pip install /root/avi-lbaasv2-*.tar.gz 
 sed -i 's/service_provider = LOADBALANCERV2:Haproxy:neutron_lbaas.drivers.haproxy.plugin_driver.HaproxyOnHostPluginDriver:default/service_provider = LOADBALANCERV2:Haproxy:neutron_lbaas.drivers.haproxy.plugin_driver.HaproxyOnHostPluginDriver\nservice_provider = LOADBALANCERV2:avi_adc:avi_lbaasv2.avi_driver.AviDriver:default/' /etc/neutron/neutron.conf

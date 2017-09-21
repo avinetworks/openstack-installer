b2ea421acee416c10ad1dfd15f211f606d2c3e12
Praveen Yalagandula <ypraveen@avinetworks.com>
2017-09-21 15:00:18 +0000

    other scripts: create nginx vm with m1.se flavor; m1.small doesn't exist by default anymore in ocata

--- others/create-nginx-vm.sh
+++ others/create-nginx-vm.sh
@@ -4,4 +4,4 @@ set -x
 #source /root/files/admin-openrc.sh
 source /root/files/demo-openrc.sh
 
-nova boot --flavor m1.small --image nginx-ssl --nic net-id=`neutron net-list | grep p2 | awk '{print $2;}'`,v4-fixed-ip=10.0.2.10 nginx1
+nova boot --flavor m1.se --image nginx-ssl --nic net-id=`neutron net-list | grep p2 | awk '{print $2;}'`,v4-fixed-ip=10.0.2.10 nginx1

57f3a733bf886b50effe3a0e684dba703c83bc08
Praveen Yalagandula <ypraveen@avinetworks.com>
2017-09-22 00:00:06 +0000

    other scripts: nginx vm create: using admin login as the Ocata neutron policy doesn't allow VM creation with specific IP address without admin privilege

--- others/create-nginx-vm.sh
+++ others/create-nginx-vm.sh
@@ -1,7 +1,9 @@
 set -e
 set -x
 
-#source /root/files/admin-openrc.sh
-source /root/files/demo-openrc.sh
+# needed to create with admin login as Ocata policies don't allow users to create ports with specific IP addresses
+source /root/files/admin-openrc.sh
+#source /root/files/demo-openrc.sh
+export OS_PROJECT_NAME=demo
 
 nova boot --flavor m1.se --image nginx-ssl --nic net-id=`neutron net-list | grep p2 | awk '{print $2;}'`,v4-fixed-ip=10.0.2.10 nginx1

a44a11a19cf77b20e5a97e8f424b6b61c35cab02
Anant Patil <anant@avinetworks.com>
2018-02-14 08:32:40 +0000

    ipv4 and ipv4 data networks

--- others/network-setup.sh
+++ others/network-setup.sh
@@ -12,18 +12,28 @@ extnetid=`openstack network show provider1 | grep " id " | awk '{print $4;}'`
 neutron router-gateway-set $routerid $extnetid
 
 # create couple of networks in admin tenant
+# mgmt network
 neutron net-create p1 --shared
 neutron subnet-create p1 10.0.1.0/24 --name p1 --dns-nameserver 10.10.0.100
 #connect router to it
 subnetid=`openstack subnet show p1 | grep " id " | awk '{print $4;}'`
 neutron router-interface-add $routerid subnet=$subnetid
 
+# vip ipv4 network
 neutron net-create p2 --shared
 neutron subnet-create p2 10.0.2.0/24 --name p2 --dns-nameserver 10.10.0.100
 #connect router to it
 subnetid=`openstack subnet show p2 | grep " id " | awk '{print $4;}'`
 neutron router-interface-add $routerid subnet=$subnetid
 
+# data ipv4 network
+neutron net-create p3 --shared
+neutron subnet-create p3 10.0.3.0/24 --name p2 --dns-nameserver 10.10.0.100
+#connect router to it
+subnetid=`openstack subnet show p3 | grep " id " | awk '{print $4;}'`
+neutron router-interface-add $routerid subnet=$subnetid
+
+# vip ipv6 network
 neutron net-create vip6 --shared
 neutron subnet-create vip6 \
     --name vip6snw \
@@ -35,6 +45,7 @@ neutron subnet-create vip6 \
 subnetid=`neutron subnet-show vip6snw -c 'id' --format 'value'`
 neutron router-interface-add $routerid subnet=$subnetid
 
+# data ipv6 network
 neutron net-create data6 --shared
 neutron subnet-create data6 \
     --name data6snw \

59d33e0cda2700aae28788b3c592a6063490ad22
Anant Patil <anant@avinetworks.com>
2018-02-15 04:19:48 +0000

    Upload trusty image from /mnt/builds instead of dowloading it

--- others/upload-image.sh
+++ others/upload-image.sh
@@ -1,10 +1,11 @@
 set -e
 set -x
 
-wget http://cloud-images.ubuntu.com/trusty/20180212/trusty-server-cloudimg-amd64-disk1.img
+# Assume the trusty image is uploaded
+# wget http://cloud-images.ubuntu.com/trusty/20180212/trusty-server-cloudimg-amd64-disk1.img
 
 # upload cirros image to glance in shared
 source /root/files/admin-openrc.sh
 
 #glance image-create --name cirros-web --disk-format qcow2 --container-format bare --file cirros-web.qcow2 --visibility public --progress
-glance image-create --name trusty --disk-format qcow2 --container-format bare --file trusty-server-cloudimg-amd64-disk1.img --visibility public --progress
+glance image-create --name trusty --disk-format qcow2 --container-format bare --file /root/trusty-server-cloudimg-amd64-disk1.img --visibility public --progress
--- others/upload-nginx-image.sh
+++ others/upload-nginx-image.sh
@@ -1,10 +1,11 @@
 set -e
 set -x
 
-wget http://cloud-images.ubuntu.com/trusty/20180212/trusty-server-cloudimg-amd64-disk1.img
+# Assume the trusty image is uploaded
+# wget http://cloud-images.ubuntu.com/trusty/20180212/trusty-server-cloudimg-amd64-disk1.img
 
 # upload cirros image to glance in shared
 source /root/files/admin-openrc.sh
 
 #glance image-create --name nginx-ssl --disk-format qcow2 --container-format bare --file nginx-ssl.qcow2 --visibility public --progress
-glance image-create --name trusty --disk-format qcow2 --container-format bare --file trusty-server-cloudimg-amd64-disk1.img --visibility public --progress
+glance image-create --name trusty --disk-format qcow2 --container-format bare --file /root/trusty-server-cloudimg-amd64-disk1.img --visibility public --progress

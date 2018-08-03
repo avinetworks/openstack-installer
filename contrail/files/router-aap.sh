42f041b4d761e90c1347d25cf48fbb84b748abdc
Anant Patil <anant@avinetworks.com>
2018-08-03 09:07:58 +0000

    Use FQDN of openstack controller instead of IP address

--- contrail/files/router-aap.sh
+++ contrail/files/router-aap.sh
@@ -1,6 +1,6 @@
 set -x
 set -e
-interface="eth0"
+interface="eth1"
 cidr="10.90.0.0/16"
 
 my_mac=`ifconfig $interface | grep "HWaddr" | awk '{print $5;}'`
@@ -11,11 +11,19 @@ fi
 
 # figure out the port-id from lab credentials
 source ./lab_openrc.sh
-# add openstack-controller to /etc/hosts
-sed -i "s/10.10.16.82 openstack-controller\n//g" /etc/hosts
-echo "10.10.16.82 openstack-controller" >> /etc/hosts
-port_id=`neutron port-list | grep "$my_mac" | awk '{print $2;}'`
 
-aaplist="mac_address=$my_mac,ip_address=$cidr"
+# Resolve openstack-controller
+sed -i "s/nameserver 10.10.0.100\n//g" /etc/resolv.conf
+echo "nameserver 10.10.0.100" >> /etc/resolv.conf
+sed -i "s/search avi.local\n//g" /etc/resolv.conf
+echo "search avi.local" >> /etc/resolv.conf
+
+port_id=`neutron port-list | grep "$my_mac" | awk '{print $2;}'`
+qrouters=`ip netns list | grep qrouter`
+aaplist=""
+for qr in $qrouters; do
+    mac=`sudo ip netns exec $qr ifconfig | grep qg | awk '{print $5;}'`
+    aaplist="$aaplist mac_address=$mac,ip_address=$cidr"
+done
 
 neutron port-update $port_id  --allowed-address-pairs type=dict list=true $aaplist 

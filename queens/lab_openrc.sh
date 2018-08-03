ec87abfb14fff090fbd2800f195b377e6628a173
Anant Patil <anant@avinetworks.com>
2018-08-03 09:23:10 +0000

    Add domain in host name, be explicit

--- queens/lab_openrc.sh
+++ queens/lab_openrc.sh
@@ -1,4 +1,4 @@
 export OS_USERNAME=admin
-export OS_AUTH_URL=http://openstack-installer:5000/v2.0
+export OS_AUTH_URL=http://openstack-installer.avi.local:5000/v2.0
 export OS_PASSWORD=avi123
 export OS_TENANT_NAME=admin

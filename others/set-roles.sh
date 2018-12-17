4fabb358d7ed73144e871345d92379755f5d782a
Anant Patil <ananta.v.patil@gmail.com>
2018-12-17 15:25:03 +0530

    Set higher quota

--- others/set-roles.sh
+++ others/set-roles.sh
@@ -4,3 +4,10 @@ set -x
 source /root/files/admin-openrc.sh
 
 openstack role add --user admin --project demo admin
+
+# Set a higher quota
+admin=`openstack project list -c ID -c Name -f value | grep -i admin | cut -d' ' -f1`
+demo=`openstack project list -c ID -c Name -f value | grep -i demo | cut -d' ' -f1`
+
+openstack quota set --networks 100 --subnets 100 --ports 100 $admin
+openstack quota set --networks 100 --subnets 100 --ports 100 $demo

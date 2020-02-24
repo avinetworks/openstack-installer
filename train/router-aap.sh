1ed719ca09b416966823a4b4b8d7dd182d1a0d2f
Rishabh Baranwal <29055492+Rishabh-Baranwal@users.noreply.github.com>
2020-02-24 11:58:02 +0530

    Train installer script (#59)

---
+++ train/router-aap.sh
@@ -0,0 +1,33 @@
+set -x
+set -e
+interface="ens4"
+cidr="10.90.0.0/16"
+
+my_mac=`cat /root/$interface | grep "ether" | awk '{print $2;}'`
+if [ -z "$my_mac" ]; then
+    echo "Can't find mac!"
+    exit
+fi
+
+# Resolve openstack-controller
+sed -i "/nameserver/d" /etc/resolv.conf
+sed -i "/search/d" /etc/resolv.conf
+echo "nameserver 10.10.0.100" >> /etc/resolv.conf
+echo "search avi.local" >> /etc/resolv.conf
+
+# Clean up any OS_ variables set
+for i in `env | grep OS_ | cut -d'=' -f1`;do unset $i;done
+
+# figure out the port-id from lab credentials
+source ./lab_openrc.sh
+
+port_id=`neutron port-list | grep "$my_mac" | awk '{print $2;}'`
+qrouters=`ip netns list | grep qrouter | cut -f 1 -d ' '`
+aaplist=""
+for qr in $qrouters; do
+    if=`ip netns exec $qr ifconfig | grep qg- | cut -d':' -f1`
+    mac=`ip netns exec $qr ifconfig $if | grep ether | awk '{print $2}'`
+    aaplist="$aaplist mac_address=$mac,ip_address=$cidr"
+done
+
+neutron port-update $port_id  --allowed-address-pairs type=dict list=true $aaplist 

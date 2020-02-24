18b109b4c23093949059fa3cd31a05df7de734cb
manikanta pagadala <35452452+manikanta0@users.noreply.github.com>
2020-02-24 17:38:54 +0530

    Removing Yes prompt for mariadb-server (#60)

--- train/installer.sh
+++ train/installer.sh
@@ -28,7 +28,7 @@ apt-get install -y ssh-client
 # install mysql
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://ftp.utexas.edu/mariadb/repo/10.3/ubuntu bionic main'
-apt-get update && apt-get install mariadb-server
+apt-get update && apt-get -y install mariadb-server
 apt-get -y install python-pymysql && service mysql restart
 mysqladmin -u root password avi123
 cp /root/files/mysqld_openstack.cnf /etc/mysql/mariadb.conf.d/99-openstack.cnf

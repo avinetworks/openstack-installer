371a9abeab05e8926710d6fdf356a3fe50bf6bee
Rishabh-Baranwal <rishabh.baranwal@avinetworks.com>
2020-05-12 17:43:42 +0530

    Added mtu option for all the nws which is now of vxlan type

--- others/network-setup.sh
+++ others/network-setup.sh
@@ -19,21 +19,21 @@ neutron router-gateway-set $router2id $extnetid
 
 # create couple of networks in admin tenant
 # mgmt network
-neutron net-create mgmt --shared
+neutron net-create mgmt --shared --mtu 1400
 neutron subnet-create mgmt 10.0.1.0/24 --name mgmtsnw
 #connect router to it
 subnetid=`openstack subnet show mgmtsnw | grep " id " | awk '{print $4;}'`
 neutron router-interface-add $routerid subnet=$subnetid
 
 # vip ipv4 network
-neutron net-create vip4 --shared
+neutron net-create vip4 --shared --mtu 1400
 neutron subnet-create vip4 10.0.2.0/24 --name vip4snw
 #connect router to it
 subnetid=`openstack subnet show vip4snw | grep " id " | awk '{print $4;}'`
 neutron router-interface-add $routerid subnet=$subnetid
 
 # data ipv4 network
-neutron net-create data4 --shared
+neutron net-create data4 --shared --mtu 1400
 neutron subnet-create data4 10.0.3.0/24 --name data4snw
 #connect router to it
 subnetid=`openstack subnet show data4snw | grep " id " | awk '{print $4;}'`
@@ -42,28 +42,28 @@ neutron router-interface-add $router2id subnet=$subnetid
 # network cidr 10.0.4.0/24 already used in testsuite test_os_cc_disrupt
 
 # vip ipv4 network
-neutron net-create vip43 --shared
+neutron net-create vip43 --shared --mtu 1400
 neutron subnet-create vip43 10.0.5.0/24 --name vip43snw
 #connect router to it
 #subnetid=`openstack subnet show vip43snw | grep " id " | awk '{print $4;}'`
 #neutron router-interface-add $routerid subnet=$subnetid
 
 # vip ipv4 network
-neutron net-create vip44 --shared
+neutron net-create vip44 --shared --mtu 1400
 neutron subnet-create vip44 10.0.6.0/24 --name vip44snw
 #connect router to it
 #subnetid=`openstack subnet show vip44snw | grep " id " | awk '{print $4;}'`
 #neutron router-interface-add $routerid subnet=$subnetid
 
 # vip ipv4 network
-neutron net-create vip45 --shared
+neutron net-create vip45 --shared --mtu 1400
 neutron subnet-create vip45 10.0.7.0/24 --name vip45snw
 #connect router to it
 #subnetid=`openstack subnet show vip45snw | grep " id " | awk '{print $4;}'`
 #neutron router-interface-add $routerid subnet=$subnetid
 
 # vip ipv6 network
-neutron net-create vip6 --shared
+neutron net-create vip6 --shared --mtu 1400
 neutron subnet-create vip6 \
     --name vip6snw \
     --ip-version 6 \
@@ -75,7 +75,7 @@ subnetid=`neutron subnet-show vip6snw -c 'id' --format 'value'`
 neutron router-interface-add $routerid subnet=$subnetid
 
 # data ipv6 network
-neutron net-create data6 --shared
+neutron net-create data6 --shared --mtu 1400
 neutron subnet-create data6 \
     --name data6snw \
     --ip-version 6 \
@@ -88,21 +88,21 @@ neutron router-interface-add $router2id subnet=$subnetid
 
 # ==== SECONDARY NETWORKS ==== #
 # vip42 ipv4 network
-neutron net-create vip42 --shared
+neutron net-create vip42 --shared --mtu 1400
 neutron subnet-create vip42 192.168.2.0/24 --name vip42snw
 # connect router to it
 # subnetid=`openstack subnet show vip4snw | grep " id " | awk '{print $4;}'`
 # neutron router-interface-add $routerid subnet=$subnetid
 
 # data42 ipv4 network
-neutron net-create data42 --shared
+neutron net-create data42 --shared --mtu 1400
 neutron subnet-create data42 192.168.3.0/24 --name data42snw
 # connect router to it
 # subnetid=`openstack subnet show data4snw | grep " id " | awk '{print $4;}'`
 # neutron router-interface-add $router2id subnet=$subnetid
 
 # vip ipv6 network
-neutron net-create vip62 --shared
+neutron net-create vip62 --shared --mtu 1400
 neutron subnet-create vip62 \
     --name vip62snw \
     --ip-version 6 \
@@ -114,7 +114,7 @@ neutron subnet-create vip62 \
 # neutron router-interface-add $routerid subnet=$subnetid
 
 # data ipv6 network
-neutron net-create data62 --shared
+neutron net-create data62 --shared --mtu 1400
 neutron subnet-create data62 \
     --name data62snw \
     --ip-version 6 \
@@ -128,7 +128,7 @@ neutron subnet-create data62 \
 
 # ==== DUAL STACK NETWORKS ==== #
 # Dual-Stack network ds1
-neutron net-create ds1 --shared
+neutron net-create ds1 --shared --mtu 1400
 neutron subnet-create ds1 \
     --name ds1snw4 \
     --ip-version 4 \
@@ -146,7 +146,7 @@ subnetid=`neutron subnet-show ds1snw6 -c 'id' --format 'value'`
 neutron router-interface-add $routerid subnet=$subnetid
 
 # Dual-Stack network ds2
-neutron net-create ds2 --shared
+neutron net-create ds2 --shared --mtu 1400
 neutron subnet-create ds2 \
     --name ds2snw4 \
     --ip-version 4 \
@@ -177,7 +177,7 @@ extnetid=`openstack network show provider1 -c 'id' -f 'value'`
 neutron router-gateway-set $routerid $extnetid
 
 # demo tenant vip ipv4 network
-neutron net-create demo-vip4
+neutron net-create demo-vip4 --mtu 1400
 neutron subnet-create demo-vip4 10.12.2.0/24 --name demo-vip4snw
 #connect router to it
 subnetid=`openstack subnet show demo-vip4snw -c 'id' -f 'value'`

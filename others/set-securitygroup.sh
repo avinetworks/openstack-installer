afd2e5f430d6e6e2e753c42f42cd180dca366024
Anant Patil <anantpatil@users.noreply.github.com>
2020-05-26 13:53:18 +0530

    AV-74160 : Update default security group (#68)
    Update default security group to not allow-all traffic

--- others/set-securitygroup.sh
+++ others/set-securitygroup.sh
@@ -7,8 +7,4 @@ source /root/files/demo-openrc.sh
 openstack security group rule create default --protocol icmpv6 --ingress --remote-ip 0::/0
 openstack security group rule create default --protocol icmp --ingress --remote-ip 0.0.0.0/0
 
-openstack security group rule create default --protocol tcp --dst-port 1:65535 --ingress --ethertype IPv4 --remote-ip 0.0.0.0/0
-openstack security group rule create default --protocol tcp --dst-port 1:65535 --ingress --ethertype IPv6 --remote-ip 0::/0
-
-openstack security group rule create default --protocol udp --dst-port 1:65535 --ingress --ethertype IPv4 --remote-ip 0.0.0.0/0
-openstack security group rule create default --protocol udp --dst-port 1:65535 --ingress --ethertype IPv6 --remote-ip 0::/0
+openstack security group rule create default --protocol tcp --dst-port 22 --ingress --ethertype IPv4 --remote-ip 0.0.0.0/0

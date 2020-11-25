8553c60f369287f67efd10fa3e39eee24cbfdc54
Anant Patil <anant@avinetworks.com>
2020-11-25 12:27:57 +0000

    Fix flavor and resource allocation ratio
    Fix flavor of SE to have 2GB RAM as required by Avi 20.1.2+
    
    Make sure the resource allocation is not over subscribed. Over
    subscription makes SE slower.

--- others/create-se-flavor.sh
+++ others/create-se-flavor.sh
@@ -4,4 +4,4 @@ set -x
 source /root/files/admin-openrc.sh
 
 openstack flavor create m1.vm --id auto --public --ram 1024 --disk 10 --vcpus 1
-openstack flavor create m1.se --id auto --public --ram 1024 --disk 20 --vcpus 1
+openstack flavor create m1.se --id auto --public --ram 2048 --disk 20 --vcpus 1

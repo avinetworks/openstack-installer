7d627b10b5587afd01f887d4fc122202b8c81c6f
Shanmugapriya Subramanian <shanmugapris@vmware.com>
2021-09-14 20:18:20 +0530

    wallaby install pkg (#93)
    * wallaby install pkg
    
    * removed id auto in create flavor cmd

--- others/create-se-flavor.sh
+++ others/create-se-flavor.sh
@@ -3,5 +3,5 @@ set -x
 
 source /root/files/admin-openrc.sh
 
-openstack flavor create m1.vm --id auto --public --ram 1024 --disk 10 --vcpus 1
-openstack flavor create m1.se --id auto --public --ram 2048 --disk 15 --vcpus 1
+openstack flavor create m1.vm --public --ram 1024 --disk 10 --vcpus 1
+openstack flavor create m1.se --public --ram 2048 --disk 15 --vcpus 1
